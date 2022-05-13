259c4c2126f198e1ad7b9986547c2c7c86e843f9
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-13 17:44:31 -0300

    update user entity

--- toro_bank_frontend/lib/modules/domain/entities/user.dart
+++ toro_bank_frontend/lib/modules/domain/entities/user.dart
@@ -2,11 +2,11 @@ class User {
   final int id;
   final String name;
   final String accountNumber;
-  final String cPF;
+  final String cpf;
   final double balance;
   final String userName;
   final String password;
 
-  User(this.id, this.name, this.accountNumber, this.cPF, this.balance,
+  User(this.id, this.name, this.accountNumber, this.cpf, this.balance,
       this.userName, this.password);
 }
