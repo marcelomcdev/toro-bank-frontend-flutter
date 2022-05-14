ddd08d402e2f19f0fed979f409f7bdbbde63353d
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-14 01:12:18 -0300

    Apply content in file app_widget

--- toro_bank_frontend/lib/main.dart
+++ toro_bank_frontend/lib/main.dart
@@ -1,22 +1,10 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:toro_bank_frontend/app_module.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
-    );
-  }
+  runApp(ModularApp(
+    module: AppModule(),
+  ));
 }
