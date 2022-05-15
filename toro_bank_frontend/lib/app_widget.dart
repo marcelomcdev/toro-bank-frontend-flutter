c2ef0d8a60ec7cb3fe2f6f52bc7e9cca229f0158
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-15 19:01:38 -0300

    Remove generated code

--- toro_bank_frontend/lib/app_widget.dart
+++ toro_bank_frontend/lib/app_widget.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key key}) : super(key: key);
+  const AppWidget({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      navigatorKey: Modular.navigatorKey,
+      title: 'Toro Bank',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      initialRoute: Modular.initialRoute,
+      onGenerateRoute: Modular.generateRoute,
     );
   }
 }
