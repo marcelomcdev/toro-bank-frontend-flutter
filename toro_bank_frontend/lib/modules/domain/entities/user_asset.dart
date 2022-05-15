2c210f530b14c08f69af26f9fdbed06287823c80
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-15 19:16:01 -0300

    Create use asset entity

---
+++ toro_bank_frontend/lib/modules/domain/entities/user_asset.dart
@@ -0,0 +1,15 @@
+class UserAsset {
+  final int id;
+  final String name;
+  final int quantity;
+  final double value;
+  final String image;
+
+  UserAsset(
+    this.id,
+    this.name,
+    this.quantity,
+    this.value,
+    this.image,
+  );
+}
