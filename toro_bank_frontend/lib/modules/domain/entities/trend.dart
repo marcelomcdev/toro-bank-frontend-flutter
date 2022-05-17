975c043021cc51963f8ffbd199829c8b99fac446
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-17 01:31:45 -0300

    add trend entity

---
+++ toro_bank_frontend/lib/modules/domain/entities/trend.dart
@@ -0,0 +1,13 @@
+class Trend {
+  final int id;
+  final String symbol;
+  final double currentPrice;
+  final String image;
+
+  Trend(
+    this.id,
+    this.symbol,
+    this.currentPrice,
+    this.image,
+  );
+}
