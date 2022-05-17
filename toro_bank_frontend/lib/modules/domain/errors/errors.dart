e51eb4d011d5640fb35047f572c2f7b119b6aeb0
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-17 01:33:56 -0300

    Add errors to asset and trends

--- toro_bank_frontend/lib/modules/domain/errors/errors.dart
+++ toro_bank_frontend/lib/modules/domain/errors/errors.dart
@@ -8,3 +8,27 @@ class DataSourceError implements UserException {
   final String message;
   DataSourceError(this.message);
 }
+
+//Assets
+abstract class AssetException implements Exception {}
+
+class InvalidAssetTextError implements AssetException {}
+
+class InvalidAssetIdentifierError implements AssetException {}
+
+class AssetDataSourceError implements AssetException {
+  final String message;
+  AssetDataSourceError(this.message);
+}
+
+//MostNegotiatedAsset
+abstract class TrendException implements Exception {}
+
+class InvalidTrendTextError implements TrendException {}
+
+class InvalidTrendIdentifierError implements TrendException {}
+
+class TrendDataSourceError implements TrendException {
+  final String message;
+  TrendDataSourceError(this.message);
+}
