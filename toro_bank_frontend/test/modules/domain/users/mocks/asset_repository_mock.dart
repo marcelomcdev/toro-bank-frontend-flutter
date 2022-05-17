606eaa66c0e96f216662c29efbd5b9a9ee070b25
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-17 01:32:50 -0300

    add asset repository mock to run tests

---
+++ toro_bank_frontend/test/modules/domain/users/mocks/asset_repository_mock.dart
@@ -0,0 +1,4 @@
+import 'package:mockito/mockito.dart';
+import 'package:toro_bank_frontend/modules/domain/repositories/asset_repository.dart';
+
+class AssetRepositoryMock extends Mock implements AssetRepository {}
