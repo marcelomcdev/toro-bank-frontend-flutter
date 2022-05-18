a69f1b402a346ea1864d28c8aef6abc2200445f1
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-18 03:35:13 -0300

    change signature of repository

--- toro_bank_frontend/lib/modules/domain/repositories/user_position_repository.dart
+++ toro_bank_frontend/lib/modules/domain/repositories/user_position_repository.dart
@@ -3,5 +3,5 @@ import 'package:toro_bank_frontend/modules/domain/entities/user_position.dart';
 import 'package:toro_bank_frontend/modules/domain/errors/errors.dart';
 
 abstract class UserPositionRepository {
-  Future<Either<UserException, UserPosition>> getById(int id);
+  Future<Either<UserException, UserPosition>> getPositions(int userId);
 }
