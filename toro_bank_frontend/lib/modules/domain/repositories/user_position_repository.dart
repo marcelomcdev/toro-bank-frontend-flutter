93fa019e8cc44467fe88ee666ea42e8523767eeb
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-18 03:34:13 -0300

    Add user position repository

---
+++ toro_bank_frontend/lib/modules/domain/repositories/user_position_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:toro_bank_frontend/modules/domain/entities/user_position.dart';
+import 'package:toro_bank_frontend/modules/domain/errors/errors.dart';
+
+abstract class UserPositionRepository {
+  Future<Either<UserException, UserPosition>> getById(int id);
+}
