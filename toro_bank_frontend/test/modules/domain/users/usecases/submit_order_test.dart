2988166f28af0b8b00ba2e0a4e6f488b105ead12
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-18 22:12:30 -0300

    ensure purchase order has a valid user id

--- toro_bank_frontend/test/modules/domain/users/usecases/submit_order_test.dart
+++ toro_bank_frontend/test/modules/domain/users/usecases/submit_order_test.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-//import 'package:mockito/mockito.dart';
+import 'package:toro_bank_frontend/modules/domain/errors/errors.dart';
 import 'package:toro_bank_frontend/modules/domain/usecases/orders/submit_order.dart';
 
 import '../mocks/order_repository_mock.dart';
@@ -19,5 +19,15 @@ main() {
         .thenAnswer((_) async => const Right(response));
     final result = await useCase(userId, symbol, amount);
     expect(result, isA<Right>());
+  });
+
+  test(
+      'should return an InvalidOrderIdentifierError error when user id is invalid',
+      () async {
+    var _userId = 0;
+    when(() => repository.submitOrder(userId, symbol, amount))
+        .thenAnswer((_) async => const Right(response));
+    final result = await useCase(_userId, symbol, amount);
+    expect(result.fold((l) => l, (r) => r), isA<InvalidOrderIdentifierError>());
   });
 }
