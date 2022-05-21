b0c38bec7987f540f4d5c0c473a00a84e8db23c9
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-21 13:50:35 -0300

    change description of tests and solve erros after changing to mocktail

--- toro_bank_frontend/test/modules/infrastructure/users/usecases/user_repository_imp_test.dart
+++ toro_bank_frontend/test/modules/infrastructure/users/usecases/user_repository_imp_test.dart
@@ -1,9 +1,6 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, import_of_legacy_library_into_null_safe
-
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-//import 'package:mockito/mockito.dart';
 import 'package:toro_bank_frontend/modules/domain/errors/errors.dart';
 import 'package:toro_bank_frontend/modules/infrastructure/models/result_user_model.dart';
 import 'package:toro_bank_frontend/modules/infrastructure/repositories/user_repository_impl.dart';
@@ -17,15 +14,14 @@ main() {
       ResultUserModel(1, "Marcelo Martins de Castro", 300123, "45358996060", 0);
   var nullValue;
   var any;
-  test('deve retornar um usuario', () async {
+  test('should return an user', () async {
     int id = 1;
     when(() => datasource.getUser(id)).thenAnswer((_) async => user);
     final result = await repository.getById(id);
     expect(result, isA<Right>());
-    //expect(result | nullValue, isA<ResultUserModel>());
   });
 
-  test('deve retornar um DataSourceError se o datasource falhar', () async {
+  test('should return a DataSourceError if datasource fails', () async {
     when(() => datasource.getUser(0)).thenThrow(Exception());
     final result = await repository.getById(0);
     expect(result.fold((l) => l, (r) => r), isA<DataSourceError>());
