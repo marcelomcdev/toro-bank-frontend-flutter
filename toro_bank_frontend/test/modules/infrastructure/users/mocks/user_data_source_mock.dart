3bf2762bff25aaba39744f1600faa9840c4c2c11
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-21 13:51:20 -0300

    Change test tool to mocktail

--- toro_bank_frontend/test/modules/infrastructure/users/mocks/user_data_source_mock.dart
+++ toro_bank_frontend/test/modules/infrastructure/users/mocks/user_data_source_mock.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: import_of_legacy_library_into_null_safe
-
 import 'package:mocktail/mocktail.dart';
-//import 'package:mockito/mockito.dart';
 import 'package:toro_bank_frontend/modules/infrastructure/datasources/user_datasource.dart';
 
 class UserDataSourceMock extends Mock implements UserDataSource {}
