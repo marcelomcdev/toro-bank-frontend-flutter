9835a5ece0ea421a7e7bb94ad25a4a2db26433be
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-22 10:12:59 -0300

    Add auth user use case

---
+++ toro_bank_frontend/lib/modules/domain/usecases/auth/auth_user.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:toro_bank_frontend/modules/domain/entities/user.dart';
+import 'package:toro_bank_frontend/modules/domain/errors/errors.dart';
+import 'package:toro_bank_frontend/modules/domain/repositories/auth_repository.dart';
+
+abstract class AuthUser {
+  Future<Either<AuthException, User?>> call(String email, String password);
+}
+
+class AuthUserImpl implements AuthUser {
+  final AuthRepository repository;
+  AuthUserImpl(this.repository);
+
+  @override
+  Future<Either<AuthException, User?>> call(
+      String email, String password) async {
+    if (email == '' || password == '') {
+      return Left(InvalidCredentialsException());
+    }
+    return repository.authUser(email, password);
+  }
+}
