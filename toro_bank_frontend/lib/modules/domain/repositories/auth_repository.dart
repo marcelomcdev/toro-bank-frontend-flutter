a5ad1927512d5d20750e9bbcafaca5ce67f04487
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-22 09:38:07 -0300

    add function to get id by token

--- toro_bank_frontend/lib/modules/domain/repositories/auth_repository.dart
+++ toro_bank_frontend/lib/modules/domain/repositories/auth_repository.dart
@@ -3,5 +3,6 @@ import 'package:toro_bank_frontend/modules/domain/entities/user.dart';
 import 'package:toro_bank_frontend/modules/domain/errors/errors.dart';
 
 abstract class AuthRepository {
-  Future<Either<UserException, User?>> AuthUser(String email, String password);
+  Future<Either<AuthException, User?>> authUser(String email, String password);
+  Future<Either<AuthException, User?>> getIdByToken(String token);
 }
