c7a796c0feaab16374af0c794acf538019c63705
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-22 09:10:18 -0300

    Add fields username and password in entity user

--- toro_bank_frontend/lib/modules/domain/entities/user.dart
+++ toro_bank_frontend/lib/modules/domain/entities/user.dart
@@ -4,12 +4,9 @@ class User {
   final int accountNumber;
   final String cpf;
   final double balance;
+  String? username;
+  String? password;
 
-  User(
-    this.id,
-    this.name,
-    this.accountNumber,
-    this.cpf,
-    this.balance,
-  );
+  User(this.id, this.name, this.accountNumber, this.cpf, this.balance,
+      {this.username, this.password});
 }
