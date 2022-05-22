97517f441a6bffc378ecb86dd813af385f3fadec
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-22 16:23:33 -0300

    add json mocks from auth tests

---
+++ toro_bank_frontend/test/modules/utils/single_auth_token.dart
@@ -0,0 +1,6 @@
+const singleAuthToken = """
+{
+  "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJqdGkiOiJkOTRhZmUzOTQyNzg0NWMzYjk0ZDIyYmE5MGI2ZjFmNyIsInJvbGUiOiJVc2VyIiwibmJmIjoxNjUzMjQ0NTA0LCJleHAiOjE2NTMyNDgxMDQsImlhdCI6MTY1MzI0NDUwNCwiaXNzIjoidG9yby1iYW5rIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzAyNSJ9.pazwIrAQewzvLDMD7KV7dnYGgoTZrLIZhziPvz-Kw5s",
+  "expires": "2022-05-22T19:35:04.6815678Z"
+}
+""";
---
+++ toro_bank_frontend/test/modules/utils/single_user_id.dart
@@ -0,0 +1,6 @@
+const singleUserId = """ 
+{
+  "id": 1
+}
+
+""";
